62417f66b57ef604636f9d94180bd259ac83cb66
Mohamed Abdelnasser <mohammed_abdelnasser@yahoo.com>
2023-05-23 23:33:43 +0300

    Initial Commit

---
+++ CarBooking/CarBooking/CarBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarBookingApp.swift
+//  CarBooking
+//
+//  Created by Mohamed Abd ElNasser on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
